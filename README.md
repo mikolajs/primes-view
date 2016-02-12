cf31b3a37972b1147a244ce1c73c9fbec9c11773
Mikołaj Sochacki <mikolajsochacki@gmail.com>
2016-02-12 11:46:37 +0100

    Add chart view

---
+++ README.md
@@ -0,0 +1,4 @@
+
+## Application for teaching
+ Check number of primes in defined ranges
+ Using Akka Actors and GUI with ScalaFX
--- src/main/scala/pl/edu/osp/Boot.scala
+++ src/main/scala/pl/edu/osp/Boot.scala
@@ -11,8 +11,7 @@ import scala.concurrent.duration._
 import scalafx.Includes._
 import scalafx.geometry.Insets
 import scalafx.scene.canvas.Canvas
-import scalafx.scene.control.{Separator, Button}
-import scalafx.scene.control.Spinner
+import scalafx.scene.control.{Label, Separator, Button, Spinner}
 import scalafx.scene.layout.{FlowPane, VBox}
 import scalafx.scene.paint.Color
 import scalafx.scene.paint.{Stops, LinearGradient}
@@ -20,7 +19,7 @@ import scalafx.application.JFXApp
 import scalafx.application.JFXApp.PrimaryStage
 import scalafx.scene.Scene
 import scalafx.scene.paint.Color._
-import scalafx.scene.text.Text
+import scalafx.scene.text.{TextAlignment, Font, Text, FontWeight}
 
 
 object Boot extends JFXApp {
@@ -32,23 +31,45 @@ object Boot extends JFXApp {
     minWidth = 100
     onAction = handle {countPrimes()}
   }
+
   val canvas = new Canvas(750, 400)
   val gc = canvas.graphicsContext2D
-  gc.fill = Color.DARKKHAKI
+  gc.fill = Color.DarkKhaki
   gc.fillRect(0, 0, canvas.width.get, canvas.height.get)
+
+  val spinner = new Spinner[Int](1000, 50000, 1000, 1000) {
+    styleClass += Spinner.StyleClassArrowsOnRightHorizontal
+    minWidth = 200
+    prefHeight = 30
+  }
+
+  val descript = (0 to 14 ).toList.map(x => {
+    new Label("") {
+      style = "-fx-font-size: 10pt"
+      minWidth = 50
+      textAlignment = TextAlignment.Center
+    }
+  })
+  val piks = (0 to 14 ).toList.map(x => {
+    new Label("") {
+      minWidth = 50
+      style = "-fx-font-size: 10pt"
+      textAlignment = TextAlignment.Center
+    }
+  })
 
   println("Start Application")
   stage = new PrimaryStage {
     title.value = "Primary View"
     width = 800
-    height = 600
+    height = 620
     scene = new Scene {
-      fill = Color.LIGHTGREEN
+      fill = Color.LightGreen
       content = new VBox {
-        fill = Color.WHITE
+        fill = Color.White
         padding = Insets(10)
         children = new VBox {
-          fill = Color.WHITE
+          fill = Color.White
           padding = Insets(10)
           children = Seq(
             new FlowPane {
@@ -59,16 +80,12 @@ object Boot extends JFXApp {
                   padding = Insets(10)
                   fill = new LinearGradient(
                     endX = 0,
-                    stops = Stops(SEAGREEN, DARKGREEN))
+                    stops = Stops(SeaGreen, DarkGreen))
                 },
                 new Separator {
                   minWidth = 50
                 },
-                new Spinner[Int](1000, 50000, 1000, 1000) {
-                  styleClass += Spinner.StyleClassArrowsOnRightHorizontal
-                  minWidth = 200
-                  prefHeight = 30
-                },
+                spinner,
                 new Separator {
                   minWidth = 50
                 },
@@ -78,7 +95,13 @@ object Boot extends JFXApp {
               minWidth = 400
               prefWidth = 400
             },
-            canvas
+            new FlowPane {
+              children = piks
+            },
+            canvas,
+            new FlowPane {
+              children = descript
+            }
           )
         }
       }
@@ -87,7 +110,7 @@ object Boot extends JFXApp {
   def countPrimes():Unit = {
 
     val system = ActorSystem("MySystem")
-    val many = 20000
+    val many = spinner.value.value
     val mainActor = system.actorOf(Props(classOf[PrimeMainActor], many ), name = "primeMActor")
     var j = 3
     val max = many*15
@@ -99,15 +122,29 @@ object Boot extends JFXApp {
     implicit val timeout = Timeout(20 seconds)
     val future: Future[Array[Int]] = ask(mainActor, Result).mapTo[Array[Int]]
     val result = Await.result(future, 10 second)
+    var k = 1
+    descript.foreach(d => {
+      d.text = (many*k).toString
+      k += 1
+    })
+      k = 0
+    piks.foreach(d => {
+      d.text = result(k).toString
+      k += 1
+    })
     println("Boot receive data: " + result.mkString(", "))
-    val colors = Array(Color.RED, Color.BLUE, Color.SILVER, Color.FUCHSIA, Color.GREEN,
-    Color.ROYALBLUE, Color.CHOCOLATE, Color.CYAN, Color.WHEAT, Color.AQUA,
-    Color.BLUEVIOLET, Color.YELLOW, Color.BROWN, Color.HOTPINK, Color.AZURE)
+    val colors = Array(Color.Red, Color.Blue, Color.Silver, Color.Fuchsia, Color.Green,
+    Color.RoyalBlue, Color.Chocolate, Color.Cyan, Color.Wheat, Color.Tan,
+    Color.BlueViolet, Color.Yellow, Color.Brown, Color.HotPink, Color.ForestGreen)
     var i = 0
+    val maxi = result.max.toDouble
+    println("MAXI = " + maxi)
     for(r <- result) {
       gc.fill = colors(i)
-      gc.fillRect(i*50, canvas.height.get - 250, 50, 250)
+      val h = 400.0*r/maxi
+      gc.fillRect(i*50, canvas.height.get - h, 50, h)
       gc.fillPath()
+
       i += 1
     }
   }
